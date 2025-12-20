dcde0ca4cea2c250854718ba716ddc89e636073b
Silletr <hristicenkoigor764@gmail.com>
2025-12-20 16:39:09 +0200

    [CHANGED FILE/DIR: backend/alarm_works.py, backend.database/alarm_info.db] Added second row to DB, increased code for interaction with DB

--- backend/alarm_works.py
+++ backend/alarm_works.py
@@ -1,33 +1,40 @@
 import sqlite3
 
-with sqlite3.connect("database/alarm_info.db", timeout=5) as db:
-    cursor = db.cursor()
-
-    # --- CREATE TABLE ---
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS alarm (
-        alarm_name TEXT PRIMARY KEY,
-        alarm_description TEXT,
-        alarm_category TEXT,
-        alarm_time TEXT,
-        alarm_day TEXT
-    )
-    """)
-
-    # --- INSERT EXAMPLE ---
-    cursor.execute("""
-INSERT OR IGNORE INTO alarm VALUES
-("Wake up", "u gotta go, now", "routine", "9:00", "everyday")
-""")
-    db.commit()
-
-    # --- SELECT ---
-    cursor.execute("SELECT * FROM alarm")
-    print(cursor.fetchall())
-
 
 def main():
-    pass
+    with sqlite3.connect(
+        "database/alarm_info.db", timeout=5, isolation_level=None
+    ) as db:
+        # Turn off WAL mode for better compatibility
+        db.execute("PRAGMA journal_mode=DELETE")
+
+        cursor = db.cursor()
+
+        # --- CREATE TABLE ---
+        cursor.execute(
+            """
+        CREATE TABLE IF NOT EXISTS alarm (
+            alarm_name TEXT PRIMARY KEY,
+            alarm_description TEXT,
+            alarm_category TEXT,
+            alarm_time TEXT,
+            alarm_day TEXT
+        )
+        """
+        )
+
+        # --- INSERT EXAMPLE ---
+        cursor.execute(
+            """
+        INSERT OR IGNORE INTO alarm VALUES
+        ("Wake up", "u gotta go, now", "routine", "9:00", "everyday")
+        """
+        )
+        db.commit()
+
+        # --- SELECT ---
+        cursor.execute("SELECT * FROM alarm")
+        print(cursor.fetchone())
 
 
 if __name__ == "__main__":
