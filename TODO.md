ab5b3e67d03a0037b76dd5c2d69b4d33b05a91a0
Silletr <hristicenkoigor764@gmail.com>
2025-12-24 18:32:26 +0200

    [CHANGED FILE/DIR: .editorconfig, TODO.md, main.py, ui/*] Started work on UI part before creating backend (for future convenience)

---
+++ TODO.md
@@ -0,0 +1,13 @@
+Done emoji = ✅
+
+**Web Version (beta-version)**:
+| Task | Is done |
+| -------------- | --------------- |
+| Realize basic UI in Web version for PC |  |
+| Realize interaction with Database for PC |  |
+| Import Web to Desktop (see next table) | |
+
+**Desktop Version (pre-release)**:
+| Task | Is done |
+| -------------- | --------------- |
+| Will be added later |  |
--- main.py
+++ main.py
@@ -1,36 +1,9 @@
 import flet as ft
 
 
-def main(page: ft.Page):
-    page.title = "Flet-Droid Calculator"
-    page.vertical_alignment = ft.MainAxisAlignment.CENTER
-    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-    page.theme_mode = ft.ThemeMode.LIGHT
-
-    txt_number = ft.TextField(
-        value="0", text_align=ft.TextAlign.RIGHT, width=100)
-
-    text_label = ft.Text(value="Press plus or minus!", size=18)
-
-    def minus_click(e):
-        txt_number.value = str(int(txt_number.value) - 1)
-        page.update()
-
-    def plus_click(e):
-        txt_number.value = str(int(txt_number.value) + 1)
-        page.update()
-
-    page.add(
-        text_label,
-        ft.Row(
-            [
-                ft.IconButton(ft.Icons.REMOVE, on_click=minus_click),
-                txt_number,
-                ft.IconButton(ft.Icons.ADD, on_click=plus_click),
-            ],
-            alignment=ft.MainAxisAlignment.CENTER,
-        )
-    )
+def main(page: ft.Page) -> None:
+    pass
 
 
-ft.app(main, view=ft.WEB_BROWSER)
+if __name__ == "__main__":
+    ft.app(main, view=ft.WEB_BROWSER)
